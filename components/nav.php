ecbb98c1932da70809bb31f3d813af95808c824d
SintiaAmanda <amndsintia@gmail.com>
2023-05-10 22:24:54 +0800

    push1

--- components/nav.php
+++ components/nav.php
@@ -6,16 +6,15 @@
       <?php endif ?>
     </a>
     <ul>
-      <li><a class="<?= $uri === '/home' ? 'bg-pink-500 hover:bg-white pb-4 pt-2 rounded-lg' : ''  ?>" href="/home">Home</a></li>
-      <li><a class="<?= $uri === '/buy' ? 'bg-pink-500 hover:bg-white pb-4 pt-2 rounded-lg' : ''  ?>" href="/buy">Buy</a></li>
-      <li><a class="<?= $uri === '/pesanan' ? 'bg-pink-500 hover:bg-white pb-4 pt-2 rounded-lg' : ''  ?>" href="/pesanan">Pesanan</a></li>
-      <li><a class="<?= $uri === '/profile' ? 'bg-pink-500 hover:bg-white pb-4 pt-2 rounded-lg' : ''  ?>" href="/profile">Profile</a></li>
-      <!-- <li><a href="login.html"> join member</a></li> -->
+      <li><a class="<?= $uri === "/home/" ? 'bg-pink-500 hover:bg-white pb-4 pt-2 rounded-lg' : ''  ?>" href="/home/">Home</a></li>
+      <li><a class="<?= $uri === "/buy/" ? 'bg-pink-500 hover:bg-white pb-4 pt-2 rounded-lg' : ''  ?>" href="/buy/">Buy</a></li>
+      <li><a class="<?= $uri === "/pesanan/" ? 'bg-pink-500 hover:bg-white pb-4 pt-2 rounded-lg' : ''  ?>" href="/pesanan/">Pesanan</a></li>
+      <li><a class="<?= $uri === "/profile/" ? 'bg-pink-500 hover:bg-white pb-4 pt-2 rounded-lg' : ''  ?>" href="/profile/">Profile</a></li>
     </ul>
     <div class="relative inline-block group w-64">
-      <span class="<?= $_SESSION['role'] === 'vip' ? 'text-gr' : 'text-white' ?>  capitalize"><?= $_SESSION['username'] ?></span>
+      <span class="font-bold <?= $_SESSION['role'] === 'VIP' ? 'text-gr' : 'text-white' ?>  capitalize"><?= $_SESSION['username'] ?></span>
       <form action="" method="POST">
-        <button name="logout" class="hidden absolute hover:bg-gray-300 left-0 text-red-500 font-semibold bg-gray-50 group-hover:block min-w-min shadow px-5 py-1 rounded-md" type="submit">Logout</button>
+        <button name="logout" class="hidden absolute hover:bg-gray-300 left-0 text-red-500 font-semibold bg-gray-50 group-hover:block min-w-min shadow px-5 py-1 rounded-md" type="submit">Log out</button>
       </form>
     </div>
   </div>
